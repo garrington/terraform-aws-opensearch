24f760d0ee0a284761b02c24b0b7c7734e6d6539
Garey Arrington <garey.arrington@vonage.com>
2024-10-09 19:27:55 -0400

    bye bye

--- index_template.tf
+++ index_template.tf
@@ -1,12 +1,12 @@
-resource "opensearch_index_template" "index_template" {
-  for_each = local.index_templates
+# resource "opensearch_index_template" "index_template" {
+#   for_each = local.index_templates
 
-  name = each.key
-  body = jsonencode(each.value)
+#   name = each.key
+#   body = jsonencode(each.value)
 
-  depends_on = [
-    opensearch_roles_mapping.master_user_arn,
-    opensearch_roles_mapping.master_user_name,
-    aws_route53_record.opensearch
-  ]
-}
+#   depends_on = [
+#     opensearch_roles_mapping.master_user_arn,
+#     opensearch_roles_mapping.master_user_name,
+#     aws_route53_record.opensearch
+#   ]
+# }
--- ism_policy.tf
+++ ism_policy.tf
@@ -1,12 +1,12 @@
-resource "opensearch_ism_policy" "ism_policy" {
-  for_each = local.ism_policies
+# resource "opensearch_ism_policy" "ism_policy" {
+#   for_each = local.ism_policies
 
-  policy_id = each.key
-  body      = jsonencode({ "policy" = each.value })
+#   policy_id = each.key
+#   body      = jsonencode({ "policy" = each.value })
 
-  depends_on = [
-    opensearch_roles_mapping.master_user_arn,
-    opensearch_roles_mapping.master_user_name,
-    aws_route53_record.opensearch
-  ]
-}
+#   depends_on = [
+#     opensearch_roles_mapping.master_user_arn,
+#     opensearch_roles_mapping.master_user_name,
+#     aws_route53_record.opensearch
+#   ]
+# }
