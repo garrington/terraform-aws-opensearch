a6c2289c0ed0eff7fd99031d9a53e5c92ac1eb4a
Garey Arrington <garey.arrington@vonage.com>
2024-10-09 19:20:16 -0400

    remove role mapping

--- role_mapping.tf
+++ role_mapping.tf
@@ -1,47 +1,47 @@
-resource "opensearch_roles_mapping" "role_mapping" {
-  for_each = {
-    for key, value in local.role_mappings :
-    key => value if !contains(["all_access", "security_manager"], key)
-  }
+# resource "opensearch_roles_mapping" "role_mapping" {
+#   for_each = {
+#     for key, value in local.role_mappings :
+#     key => value if !contains(["all_access", "security_manager"], key)
+#   }
 
-  role_name     = each.key
-  description   = try(each.value.description, "")
-  backend_roles = try(each.value.backend_roles, [])
-  hosts         = try(each.value.hosts, [])
-  users         = try(each.value.users, [])
+#   role_name     = each.key
+#   description   = try(each.value.description, "")
+#   backend_roles = try(each.value.backend_roles, [])
+#   hosts         = try(each.value.hosts, [])
+#   users         = try(each.value.users, [])
 
-  depends_on = [
-    opensearch_role.role,
-    aws_route53_record.opensearch
-  ]
-}
+#   depends_on = [
+#     opensearch_role.role,
+#     aws_route53_record.opensearch
+#   ]
+# }
 
-resource "opensearch_roles_mapping" "master_user_arn" {
-  for_each = var.advanced_security_options_internal_user_database_enabled ? {} : {
-    for key in ["all_access", "security_manager"] :
-    key => try(local.role_mappings[key], {})
-  }
+# resource "opensearch_roles_mapping" "master_user_arn" {
+#   for_each = var.advanced_security_options_internal_user_database_enabled ? {} : {
+#     for key in ["all_access", "security_manager"] :
+#     key => try(local.role_mappings[key], {})
+#   }
 
-  role_name     = each.key
-  description   = try(each.value.description, "")
-  backend_roles = concat(try(each.value.backend_roles, []), [var.master_user_arn])
-  hosts         = try(each.value.hosts, [])
-  users         = try(each.value.users, [])
+#   role_name     = each.key
+#   description   = try(each.value.description, "")
+#   backend_roles = concat(try(each.value.backend_roles, []), [var.master_user_arn])
+#   hosts         = try(each.value.hosts, [])
+#   users         = try(each.value.users, [])
 
-  depends_on = [aws_route53_record.opensearch]
-}
+#   depends_on = [aws_route53_record.opensearch]
+# }
 
-resource "opensearch_roles_mapping" "master_user_name" {
-  for_each = var.advanced_security_options_internal_user_database_enabled ? {
-    for key in ["all_access", "security_manager"] :
-    key => try(local.role_mappings[key], {})
-  } : {}
+# resource "opensearch_roles_mapping" "master_user_name" {
+#   for_each = var.advanced_security_options_internal_user_database_enabled ? {
+#     for key in ["all_access", "security_manager"] :
+#     key => try(local.role_mappings[key], {})
+#   } : {}
 
-  role_name     = each.key
-  description   = try(each.value.description, "")
-  backend_roles = try(each.value.backend_roles, [])
-  hosts         = try(each.value.hosts, [])
-  users         = concat(try(each.value.users, []), [var.advanced_security_options_master_user_name])
+#   role_name     = each.key
+#   description   = try(each.value.description, "")
+#   backend_roles = try(each.value.backend_roles, [])
+#   hosts         = try(each.value.hosts, [])
+#   users         = concat(try(each.value.users, []), [var.advanced_security_options_master_user_name])
 
-  depends_on = [aws_route53_record.opensearch]
-}
+#   depends_on = [aws_route53_record.opensearch]
+# }
